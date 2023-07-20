ab7a1a59a6dc88aacacf4245ca473542288fb96e
Pranavkarthikeya1 <113463194+Pranavkarthikeya1@users.noreply.github.com>
2023-07-20 15:47:25 +0530

    Add files via upload

---
+++ c111/test1.py
@@ -0,0 +1,18 @@
+import os
+
+source="main.txt"
+dest="newfile.txt"
+
+os.rename(source)
+
+a=33
+b="33"
+
+if b>a:
+    print("b is greater than a")
+
+elif a == b:
+    print("a and b are equal")
+
+
+
